1c7fc140b38b11ef8972ac77dabb384cfbedff80
cdra0624 <t.adaniya@adawarp.com>
2019-04-24 08:14:31 +0900

    fix: typo PORT to port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 5420;
+const port = process.env.PORT || 5420;
 
 const db = require('./queries');
 
