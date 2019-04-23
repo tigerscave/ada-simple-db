adab76eee8ff0476225ba2ab4eca9a27623c18a0
cdra0624 <t.adaniya@adawarp.com>
2019-04-24 08:09:28 +0900

    chore: change port and add run script for heroku deploy

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5420;
+const PORT = process.env.PORT || 5420;
+
 const db = require('./queries');
 
 app.use(bodyParser.json());
